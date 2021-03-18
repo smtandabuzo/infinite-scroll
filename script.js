a535214811b0d60aa61c3fbc141ce2e5a14345c5
Sazi <s_mtandabuzo@yahoo.com>
2021-03-18 07:28:25 +0200

    Improve performance

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
+let imageCount = 5;
 const apiKey = 'w24vtTNwGoOplpHsisODNLHxgXcXSZIArJBFQMjNiZM';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -17,6 +17,7 @@ function imageLoaded() {
   if (imagesLoaded === totalImages) {
     ready = true;
     loader.hidden = true;
+    imageCount = 30;
   }
 }
 
